77f10f182de5a8e86a900324bd2543c38a288378
julunaedu <julunaedu@gmail.com>
2025-08-29 10:48:34 -0300

    add ContaTerminal.java

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        // TODO: Conhecer e importar a classe Scanner
+        Scanner input = new Scanner(System.in);
+
+        // TODO: Exibir as mensagens para o usuário
+        // TODO: Obter pela Scanner os valores digitados no Terminal
+        System.out.println("Digite o seu nome:");
+        String nomeCliente = input.nextLine();
+
+        System.out.println("Digite o número de sua agência:");
+        String agencia = input.nextLine();
+
+        System.out.println("Digite o número de sua conta:");
+        int numeroConta = input.nextInt();
+
+        System.out.println("Digite o seu saldo:");
+        double saldo = input.nextDouble();
+
+        // TODO: Exibir a mensagem da conta criada
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.", nomeCliente, agencia, numeroConta, saldo);
+    }
+}
